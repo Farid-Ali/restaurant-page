22ca5dbc067280d35c9dbd34e0fcfa53827a431d
Farid Ali <faridalicode@gmail.com>
2021-10-11 21:21:46 +0530

    setup webpack basic configaration file

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,36 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
+  //mode of the development
+  mode: "development",
+  //Source map for debuging purpose
+  devtool: "inline-source-map",
+  entry: {
+    index: "./src/index.js",
+  },
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    //clean the dist directory in every build
     clean: true,
   },
-  devtool: 'inline-source-map',
   plugins: [
+    //automatically generate html file and add script tag dyanamically to it
     new HtmlWebpackPlugin({
-      title: 'Restaurant Page',
+      title: "Restaurant Page",
     }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      }
+        type: "asset/resource",
+      },
     ],
   },
 };
